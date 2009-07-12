1dc920ada2db4464328c8a843cf7d08a7f2e8ac3
kematzy <kematzy@gmail.com>
2009-07-12 12:50:02 +0800

    Added a bit of test outline of common parts.

--- lib/templates/is/spec/integration/%snake_name%_spec.rb
+++ lib/templates/is/spec/integration/%snake_name%_spec.rb
@@ -3,6 +3,23 @@ require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
 
 if HAS_SQLITE3 || HAS_MYSQL || HAS_POSTGRES
   describe 'DataMapper::Is::<%= class_name %>' do
-
-  end
-end
+    
+    before(:each) do 
+      # bootstrap
+    end
+    
+    it "should have some sort of tests at least" do
+      true.should == false
+    end
+    
+    describe "Class Methods" do 
+      
+    end #/ Class Methods
+    
+    describe "Instance Methods" do 
+      
+    end #/ Instance Methods
+    
+  end #/ DataMapper::Is::<%= class_name %>
+  
+end #/ if
